8df15d673e3822ffbc00f64c2e2a74f8e96d4c41
John Vilela <johnvvilela@gmail.com>
2026-01-12 05:06:19 -0300

    feat: redirecionar user se estiver logado para o dashboard

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,12 @@
 import { TanStackDevtools } from "@tanstack/react-devtools";
 import type { QueryClient } from "@tanstack/react-query";
-import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
+import {
+	createRootRouteWithContext,
+	Outlet,
+	redirect,
+} from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import { isAuthenticated } from "@/lib/modules/auth/auth.service";
 import TanStackQueryDevtools from "../contexts/tanstack-query/devtools";
 
 interface MyRouterContext {
@@ -9,6 +14,16 @@ interface MyRouterContext {
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
+	beforeLoad: ({ location }) => {
+		const authenticated = isAuthenticated();
+		const publicPaths = ["/", "/login"];
+
+		if (authenticated && publicPaths.includes(location.pathname)) {
+			throw redirect({
+				to: "/dashboard",
+			});
+		}
+	},
 	component: () => (
 		<>
 			<Outlet />
